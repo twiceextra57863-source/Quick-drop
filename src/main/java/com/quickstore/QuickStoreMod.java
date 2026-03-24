791368b20d18cf456f36901e4c29e22cf560133a
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-24 15:28:48 +0530

    Generate src/main/java/com/quickstore/QuickStoreMod.java via Flame AI

---
+++ src/main/java/com/quickstore/QuickStoreMod.java
@@ -0,0 +1,15 @@
+package com.quickstore;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuickStoreMod implements ModInitializer {
+    public static final String MOD_ID = "quickstore";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("QuickStoreMod initialized!");
+    }
+}

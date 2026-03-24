3268787c4f0491d22a8479ccaeec129741a86213
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-25 00:12:39 +0530

    Update QuickToggleButton.java

--- src/main/java/com/quickchest/gui/QuickToggleButton.java
+++ src/main/java/com/quickchest/gui/QuickToggleButton.java
@@ -4,17 +4,14 @@ import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
 
 public class QuickToggleButton {
-
-    public static ButtonWidget create(
-        int x, int y, int width, int height,
-        Text message,
-        ButtonWidget.PressAction onPress
-    ) {
-        return ButtonWidget.builder(message, onPress)
-            .dimensions(x, y, width, height)
-            .tooltip(net.minecraft.client.gui.tooltip.Tooltip.of(
-                Text.literal("Toggle QuickChest: drop + store on chest click")
-            ))
-            .build();
+    
+    public static ButtonWidget create(int x, int y, int width, int height, 
+                                       ButtonWidget.PressAction onPress) {
+        return ButtonWidget.builder(
+            Text.literal("Quick Chest: OFF"),
+            onPress
+        )
+        .dimensions(x, y, width, height)
+        .build();
     }
 }

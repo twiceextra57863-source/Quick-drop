58220a47b02e35b3811aa5283a2c7984c302c557
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-24 15:49:04 +0530

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,13 +1,1 @@
-@echo off
-
-set DEFAULT_JVM_OPTS="-Xmx1024m" "-Xms256m"
-
-if "%JAVA_HOME%"=="" (
-    set JAVA_EXE=java
-) else (
-    set JAVA_EXE=%JAVA_HOME%\bin\java.exe
-)
-
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
-
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+java -Xmx1024M -jar gradle/wrapper/gradle-wrapper.jar %*

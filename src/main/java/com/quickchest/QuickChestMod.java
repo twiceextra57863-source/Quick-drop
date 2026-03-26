2196b8d7fa18a90105bd7aeddee3a190bb327884
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-26 22:52:51 +0530

    Update and rename QuickChest.java to QuickChestMod.java

--- src/main/java/com/quickchest/QuickChestMod.java
+++ src/main/java/com/quickchest/QuickChestMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class QuickChest implements ModInitializer {
+public class QuickChestMod implements ModInitializer { // <--- Class name must be QuickChestMod
     public static final String MOD_ID = "quickchest";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 

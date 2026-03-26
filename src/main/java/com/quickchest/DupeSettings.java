d733ebbe53d4e5598107b48714c9d5c203d12474
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-26 23:14:44 +0530

    Update DupeSettings.java

--- src/main/java/com/quickchest/DupeSettings.java
+++ src/main/java/com/quickchest/DupeSettings.java
@@ -1,5 +1,13 @@
 package com.quickchest;
 
 public class DupeSettings {
-    public static boolean isEnabled = false;
+    public static int dupeMode = 0; // 0 = OFF, 1 = Auto-Hopper, 2 = Packet-Drop (New)
+    
+    public static String getModeName() {
+        return switch (dupeMode) {
+            case 1 -> "§aAuto-Hopper";
+            case 2 -> "§bPacket-Drop";
+            default -> "§cOFF";
+        };
+    }
 }

b2a85f659c1f6b3a2e65728f253173798d5322d6
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 11:25:19 +0530

    Create TClientMod.java

---
+++ src/main/resources/java/com/yourmod/TClientMod.java
@@ -0,0 +1,15 @@
+package com.yourmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TClientMod implements ModInitializer {
+    public static final String MOD_ID = "tclient";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("T Client Mod initialized!");
+    }
+}

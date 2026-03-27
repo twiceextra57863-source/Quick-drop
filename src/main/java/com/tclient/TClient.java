86c2a76cde011baef346364a30691de5aea2b650
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 12:30:09 +0530

    Update TClient.java

--- src/main/java/com/tclient/TClient.java
+++ src/main/java/com/tclient/TClient.java
@@ -1,15 +1,11 @@
 package com.tclient;
 
+import com.tclient.font.FontManager;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TClient implements ModInitializer {
-    public static final String MOD_ID = "tclient";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
     @Override
     public void onInitialize() {
-        LOGGER.info("T Client Initialized!");
+        FontManager.init(); // Font list initialize karega
     }
 }

2e69644328748eb44f4a5d16c103573bc1650716
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 13:25:33 +0530

    Create TClientModClient.java

---
+++ src/main/Java/com/tclient/mod/TClientModClient.java
@@ -0,0 +1,32 @@
+package com.tclient.mod;
+
+import com.tclient.mod.gui.TClientScreen;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
+
+public class TClientModClient implements ClientModInitializer {
+
+    public static KeyBinding openMenuKey;
+
+    @Override
+    public void onInitializeClient() {
+        openMenuKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.tclient.open_menu",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                "category.tclient"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openMenuKey.wasPressed()) {
+                if (client.currentScreen == null) {
+                    client.setScreen(new TClientScreen());
+                }
+            }
+        });
+    }
+}

af8b15859d3858ac44adb2d07937bdebcd34785f
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 11:59:28 +0530

    Update FontManager.java

--- src/main/java/com/yourmod/utils/FontManager.java
+++ src/main/java/com/yourmod/utils/FontManager.java
@@ -2,32 +2,19 @@ package com.yourmod.utils;
 
 import com.yourmod.TClientMod;
 import net.minecraft.client.MinecraftClient;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.awt.*;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class FontManager {
-    private static final Logger LOGGER = LoggerFactory.getLogger(FontManager.class);
-    
     public static void applyFont(String fontName) {
         try {
             MinecraftClient client = MinecraftClient.getInstance();
-            LOGGER.info("Applying font: {}", fontName);
-            
-            // In a real implementation, you would need to override Minecraft's font rendering
-            // This is a placeholder for now
+            TClientMod.LOGGER.info("Applying font: {}", fontName);
+            // Font implementation would go here
         } catch (Exception e) {
-            LOGGER.error("Failed to apply font", e);
+            TClientMod.LOGGER.error("Failed to apply font", e);
         }
     }
     
     public static void resetFont() {
-        LOGGER.info("Resetting to default font");
+        TClientMod.LOGGER.info("Resetting to default font");
     }
 }

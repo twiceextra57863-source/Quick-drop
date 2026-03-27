e0117f43ab0a9956a7fb2d477ad089efbaa743a4
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 12:39:49 +0530

    Update FontMixin.java

--- src/main/java/com/tclient/mixin/FontMixin.java
+++ src/main/java/com/tclient/mixin/FontMixin.java
@@ -9,9 +9,19 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(TextRenderer.class)
 public class FontMixin {
-    
+
+    // Minecraft 1.21.4 draw method redirection
     @ModifyVariable(method = "draw(Lnet/minecraft/text/OrderedText;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I", at = @At("HEAD"), argsOnly = true)
-    private Identifier changeFont(Identifier identifier) {
+    private Identifier redirectFont(Identifier identifier) {
+        if (FontManager.currentFont != null && !FontManager.currentFont.equals("Default")) {
+            return FontManager.getFontIdentifier();
+        }
+        return identifier;
+    }
+
+    // Secondary draw method for plain strings
+    @ModifyVariable(method = "draw(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I", at = @At("HEAD"), argsOnly = true)
+    private Identifier redirectFontString(Identifier identifier) {
         if (FontManager.currentFont != null && !FontManager.currentFont.equals("Default")) {
             return FontManager.getFontIdentifier();
         }

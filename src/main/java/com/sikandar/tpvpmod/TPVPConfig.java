06b50ce7fdd6cb209b943311475c07dafca68188
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 21:21:25 +0530

    Update TPVPConfig.java

--- src/main/java/com/sikandar/tpvpmod/TPVPConfig.java
+++ src/main/java/com/sikandar/tpvpmod/TPVPConfig.java
@@ -2,12 +2,13 @@ package com.sikandar.tpvpmod;
 
 public class TPVPConfig {
     public static boolean healthIndicatorEnabled = false;
-    public static boolean showOnSelf = true;           // apna health bhi dikhe ya sirf opponent
-    public static HealthStyle style = HealthStyle.PROGRESS_BAR;
+    public static boolean showOnSelf = true;
 
     public enum HealthStyle {
         PROGRESS_BAR,
         HEARTS,
         HEAD_WITH_NUMBER
     }
+
+    public static HealthStyle style = HealthStyle.PROGRESS_BAR;
 }

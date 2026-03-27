6de57835c3a9b8285a5fc86639d8b071d7322194
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 12:24:38 +0530

    Update FontManager.java

--- src/main/java/com/tclient/font/FontManager.java
+++ src/main/java/com/tclient/font/FontManager.java
@@ -1,16 +1,56 @@
 package com.tclient.font;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FontManager {
-    public static String currentFont = "default"; // Options: default, modern, smooth, pixel
+    public static String currentFont = "Default";
+    public static final List<String> availableFonts = new ArrayList<>();
+    private static final File FONT_DIR = new File(FabricLoader.getInstance().getConfigDir().toFile(), "T-Client/fonts");
+
+    static {
+        // Default Fonts List
+        availableFonts.add("Default");
+        availableFonts.add("Modern");
+        availableFonts.add("Smooth");
+        availableFonts.add("Minecraftia");
+        availableFonts.add("Roboto");
+        availableFonts.add("Arial");
+        availableFonts.add("Impact");
+        availableFonts.add("Verdana");
+        availableFonts.add("ComicSans");
+        availableFonts.add("Pixel-Pro");
+        availableFonts.add("Ghost");
+        
+        // Create folder if not exists
+        if (!FONT_DIR.exists()) {
+            FONT_DIR.mkdirs();
+        }
+        
+        // Scan for external .ttf files
+        loadExternalFonts();
+    }
+
+    public static void loadExternalFonts() {
+        File[] files = FONT_DIR.listFiles((dir, name) -> name.toLowerCase().endsWith(".ttf"));
+        if (files != null) {
+            for (File file : files) {
+                String name = file.getName().replace(".ttf", "");
+                if (!availableFonts.contains(name)) {
+                    availableFonts.add("[Custom] " + name);
+                }
+            }
+        }
+    }
 
     public static Identifier getFontIdentifier() {
-        if (currentFont.equals("modern")) {
-            return Identifier.of("tclient", "modern_font");
-        } else if (currentFont.equals("smooth")) {
-            return Identifier.of("tclient", "smooth_font");
-        }
-        return Identifier.of("minecraft", "default");
+        if (currentFont.equals("Default")) return Identifier.of("minecraft", "default");
+        
+        // Convert name to lowercase for Identifier (Minecraft requirement)
+        String fontId = currentFont.toLowerCase().replace(" ", "_").replace("[custom]_", "");
+        return Identifier.of("tclient", fontId);
     }
 }

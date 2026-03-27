6cb7ffdd91d860f5d4a0130263e974fd074f0224
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 13:10:38 +0530

    Update FontMixin.java

--- src/main/java/com/tclient/mixin/FontMixin.java
+++ src/main/java/com/tclient/mixin/FontMixin.java
@@ -1,20 +1,16 @@
 package com.tclient.mixin;
 
-import com.tclient.font.FontManager;
-import net.minecraft.client.font.FontManager; // Minecraft's FontManager
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import net.minecraft.util.Identifier;
 
 @Mixin(net.minecraft.client.font.FontManager.class)
 public class FontMixin {
 
-    // Minecraft jab font list dhoondta hai, hum wahi par apna custom ID ghusa denge
     @ModifyVariable(method = "method_27539", at = @At("HEAD"), argsOnly = true, remap = false)
     private Identifier changeFontOnLoad(Identifier id) {
         if (com.tclient.font.FontManager.currentFont != null && !com.tclient.font.FontManager.currentFont.equals("Default")) {
-            // Agar T-Client mein font selected hai toh default font ko replace kar do
             if (id.getNamespace().equals("minecraft") && id.getPath().equals("default")) {
                 return com.tclient.font.FontManager.getFontIdentifier();
             }

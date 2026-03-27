2fed42b80f70c21de5b27e94bf4bce7ed6a3c519
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 21:24:16 +0530

    Update TitleScreenMixin.java

--- src/main/java/com/sikandar/tpvpmod/mixin/TitleScreenMixin.java
+++ src/main/java/com/sikandar/tpvpmod/mixin/TitleScreenMixin.java
@@ -10,17 +10,19 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class TitleScreenMixin {
+public abstract class TitleScreenMixin extends net.minecraft.client.gui.screen.Screen {
 
-    @Inject(method = "initWidgetsNormal", at = @At("RETURN"))
+    protected TitleScreenMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "addNormalWidgets", at = @At("RETURN"))
     private void addTPVPButton(int y, int spacingY, CallbackInfo ci) {
-        TitleScreen screen = (TitleScreen) (Object) this;
-
         ButtonWidget tpvpButton = ButtonWidget.builder(
                 Text.literal("§cT PVP"),
-                button -> screen.client.setScreen(new TPVPDashboardScreen())
-        ).dimensions(screen.width / 2 - 100, y + spacingY * 3, 200, 20).build();  // position adjust kar sakta hai
+                button -> this.client.setScreen(new TPVPDashboardScreen())
+        ).dimensions(this.width / 2 - 100, y + spacingY * 3, 200, 20).build();   // position adjust kar sakta hai (*3 ya *4 try kar)
 
-        screen.addDrawableChild(tpvpButton);
+        this.addDrawableChild(tpvpButton);
     }
 }

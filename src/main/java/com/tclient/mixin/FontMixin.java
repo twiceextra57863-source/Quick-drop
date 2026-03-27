8ffc358d6be090888f64886436060eb1deaefcca
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 12:35:55 +0530

    Update FontMixin.java

--- src/main/java/com/tclient/mixin/FontMixin.java
+++ src/main/java/com/tclient/mixin/FontMixin.java
@@ -10,11 +10,9 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(TextRenderer.class)
 public class FontMixin {
     
-    // Ye method text render hone se pehle font ID ko badal deta hai
-    @ModifyVariable(method = "draw(Lnet/minecraft/text/OrderedText;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I", at = @At("HEAD"), ordinal = 0)
+    @ModifyVariable(method = "draw(Lnet/minecraft/text/OrderedText;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/font/TextRenderer$TextLayerType;II)I", at = @At("HEAD"), argsOnly = true)
     private Identifier changeFont(Identifier identifier) {
-        // Agar font "default" nahi hai toh humara custom font return karega
-        if (!FontManager.currentFont.equals("default")) {
+        if (FontManager.currentFont != null && !FontManager.currentFont.equals("Default")) {
             return FontManager.getFontIdentifier();
         }
         return identifier;

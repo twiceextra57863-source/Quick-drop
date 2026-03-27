0c32f45e317d1db3afedfe55fa6c8890768b3ead
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-27 15:32:35 +0530

    Update TitleScreenMixin.java

--- src/main/java/com/tclient/mod/mixin/TitleScreenMixin.java
+++ src/main/java/com/tclient/mod/mixin/TitleScreenMixin.java
@@ -1,7 +1,6 @@
 package com.tclient.mod.mixin;
 
 import com.tclient.mod.gui.TClientScreen;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
@@ -11,25 +10,22 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public abstract class TitleScreenMixin extends Screen {
+public class TitleScreenMixin extends net.minecraft.client.gui.screen.Screen {
 
-    // Mixin classes abstract honi chahiye aur constructor ki jarurat nahi hoti
-    protected TitleScreenMixin(Text title) {
-        super(title);
+    protected TitleScreenMixin() { 
+        super(Text.literal("Title Screen")); 
     }
 
-    // 1.21.4 mein "init" method use karna sabse safe hai
     @Inject(method = "init", at = @At("TAIL"))
     private void onInit(CallbackInfo ci) {
         int btnW = 80;
         int btnH = 20;
         int btnX = this.width - btnW - 10;
         int btnY = 10;
-
-        // Button build karke add karna
+        
         this.addDrawableChild(
             ButtonWidget.builder(
-                Text.literal("§b■ T Client"), 
+                Text.literal("§b■ T CLIENT"),
                 button -> {
                     if (this.client != null) {
                         this.client.setScreen(new TClientScreen());
